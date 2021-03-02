98bbca2ce6b15aa032c440af4a406b0cb1deb9cd
Siar Sohrab <siar.sohrab@gmail.com>
2021-03-02 12:40:47 +0100

    done ex-1

--- ex-1/README.md
+++ ex-1/README.md
@@ -1,5 +1,7 @@
 # Change color with one click
 
-Write a JavaScript program to set the background color of all the paragraphs.
+///Write a JavaScript program to set the background color of all the paragraphs.
 
-Use different colors for each p tag.
+//Use different colors for each p tag.
+
+
--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,5 +1,9 @@
 // Use this function that is linked to the button to change the background
 
 function set_background() {
+    
+     document.getElementById("first").style.backgroundColor = " blue";
+     document.getElementById("second").style.backgroundColor = "lightblue";
+     document.getElementById("third").style.backgroundColor = " magenta";
 
 }
