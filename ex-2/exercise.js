0a2c7ad3e530ac05cf5027d71d259d4dd2f5457a
Siar Sohrab <siar.sohrab@gmail.com>
2021-03-02 15:17:34 +0100

    done ex-2

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,14 @@
 function getAttributes() {
+    
+   var type = document.getElementsByTagName("a")[0].getAttribute("type");
 
+   var hreflang = document.getElementsByTagName("a")[0].getAttribute("hreflang");
+
+   var rel = document.getElementsByTagName("a")[0].getAttribute("rel");
+
+   var target = document.getElementsByTagName("a")[0].getAttribute("target");
+
+
+   console.log(type+" "+ hreflang+ " "+ rel+ " "+target );
 }
+
